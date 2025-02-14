67e44b49adb1699dbeec500df0f010725fc15a95
Mitch Tuck <matuck@matuck.com>
2025-02-14 10:21:33 -0500

    add the ability to install packages and give a selectable on what packages should be installed

--- README.md
+++ README.md
@@ -4,14 +4,10 @@ dotfiles
 This repo is orginally based off of [How I manage my dotfiles using GNU Stow](https://dev.to/spacerockmedia/how-i-manage-my-dotfiles-using-gnu-stow-4l59).  There may be some adaptions over time.  I will do my best to keep the readme updated.
 
 # Setup
-To setup your dotfiles you will need to run 2 scripts on your first setup.  
+To setup your dotfiles you will need to run the setup.sh script.  If the distribution has already been configured it will install any packages the script needs to run provided you tell the script its ok to install packages.  It will then install any packages needed for any specific dotfiles you tell it to install.  This is provided that in the __setup directory there is a file for each dotfile folder with a .<Distro> tag.
 
-##  __setup/<distribution>.sh
-Distribution will be the distribution of that system.  If this script does not exist you, may need to create it based on the other scripts.  This will install any neccessary packages.
-## setup.sh
-This script will actualy create the links in your home directory.
 
 # Updates
-After updating any files be sure to add and commit to the repository, so that the changes can be propegated to other systems.
+After updating any files be sure to add and commit to the repository, so that the changes can be propagated to other systems.
 
 You may need to rerun the setup.sh script from time to time to update any new information that may be created.
--- setup.sh
+++ setup.sh
@@ -1,24 +1,47 @@
 #!/usr/bin/env bash
 
+# Set distrubution
+DIST=$(lsb_release -i|sed 's/Distributor ID:\t//g')
+echo $DIST
+#DIST="Debian"
+read -p "Do you want to install packages (y|n): " -i y -e DOPKGINSTALLS;
+
+case "$DIST" in 
+    Arch)
+        pacman -Qi yay &> /dev/null
+        retVal=$?
+        if [ $retVal -eq 1 ] ; then
+            echo "Please install yay";
+        fi
+        PKGINST="yay -S - ";
+        ;;
+    "Ubuntu" | "Debian")
+        echo "This is not setup yet"
+        exit 1;
+        ;;
+    *)
+        echo "Your distrubution is not supported.";
+        exit 1;
+        ;;
+esac
+if [[ "$DOPKGINSTALLS" = "y" ]] ; then
+    if [ -f __setup/__pre.$DIST ] ; then
+        eval "$PKGINST < __setup/__pre.$DIST";
+    fi
+fi
+
 # make sure we have pulled in and updated any submodules
 git submodule init
 git submodule update
-
-# what directories should be installable by all users including the root user
-base=(
-    bash
-)
-
-# folders that should, or only need to be installed for a local user
-useronly=(
-    bin
-    vim
-    nvim
-    git
-    tmux
-    backgrounds
-    hyprland_desktop
-)
+cmd=(dialog --separate-output --checklist "Select options:" 22 76 17)
+options2=("ss1" "" off
+    2 "Option 2" off
+    3 "Option 3" off
+    4 "Option 4" off
+    )
+options=($(ls -d */|sed 's/\///'|grep -v __setup|sed 's/$/ files on/'));
+DOTINSTALLS=$("${cmd[@]}" "${options[@]}" 2>&1 > /dev/tty);
+clear;
 
 # run the stow command for the passed in directory ($2) in location $1
 stowit() {
@@ -33,16 +56,16 @@ stowit() {
 echo ""
 echo "Stowing apps for user: ${whoami}"
 
-# install apps available to local users and root
-for app in ${base[@]}; do
-    stowit "${HOME}" $app 
-done
-
-# install only user space folders
-for app in ${useronly[@]}; do
-    if [[ ! "$(whoami)" = *"root"* ]]; then
-        stowit "${HOME}" $app 
+# install apps available to local users
+for app in $DOTINSTALLS; do
+    if [[ "$DOPKGINSTALLS" = "y" ]] ; then
+        echo "__setup/$app.$DIST";
+        if [ -f __setup/$app.$DIST ] ; then
+            echo "Install any packages needed for $app dot files";
+            eval "$PKGINST < __setup/$app.$DIST";
+        fi
     fi
+    stowit "${HOME}" $app; 
 done
 
 echo ""
